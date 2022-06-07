fa98b3a1c95862a5a0300c991645accd3aea317c
Michelle Clement <michellelclement@gmail.com>
2022-06-07 13:52:56 +0000

    Revert back input animation if no input made or selected

--- app.js
+++ app.js
@@ -39,3 +39,24 @@ containers.forEach((container) => {
     }
   });
 });
+
+//Revert back if not focussed
+form.addEventListener("click", () => {
+  containers.forEach((container) => {
+    const input = container.querySelector(".input");
+    const line = container.querySelector(".elastic-line");
+    const placeholder = container.querySelector(".placeholder");
+
+    if (document.activeElement !== input) {
+      if (!input.value) {
+        gsap.to(placeholder, {
+          top: 0,
+          left: 0,
+          scale: 1,
+          duration: 0.5,
+          ease: "Power2.easeOut"
+        });
+      }
+    }
+  });
+});
